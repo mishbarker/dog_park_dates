a64086a37356577f2deb7de3886e72cda215bea0
Michelle Barker <barkerwebdev47@gmail.com>
2020-07-24 10:52:50 -0700

    branch test for group

--- apps/dog_park_app/views.py
+++ apps/dog_park_app/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import *
 
-# def index(request):
-#     pass
+def index(request):
+    pass
     # return render(request, 'index.html')

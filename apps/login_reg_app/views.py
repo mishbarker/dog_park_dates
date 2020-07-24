46a9f6c13fc414a7ec2ae470326483bdf16b9625
Michelle Barker <barkerwebdev47@gmail.com>
2020-07-23 19:52:19 -0700

    added login-reg views.py from Irina

--- apps/login_reg_app/views.py
+++ apps/login_reg_app/views.py
@@ -1,51 +1,38 @@
 from django.shortcuts import render, redirect
+import bcrypt
 from django.contrib import messages
 from .models import *
 
-def index(request): #GET
-    return render(request, "index.html")
-
-def register(request): 
-    # if request.method == 'POST': #POST
-    #     errors = User.objects.validator(request.POST)
-
-    #     if len(errors) > 0:
-    #         for k, v in errors.items():
-    #             messages.error(request, v)
-    #         return redirect("/")
-
-    #     else:
-    #         pw_hash = bcrypt.hashpw(request.POST['pw_form'].encode(), bcrypt.gensalt()).decode()
-
-    #         print(pw_hash)
-            
-    #         user = User.objects.create(
-    #             first_name=request.POST['firstName_form'], last_name=request.POST['lastName_form'],
-    #             email=request.POST['email_form'],
-    #             password=pw_hash
-    #         ) 
-
-    #         print(user)
-    #         request.session['user_id']=user.id 
-
-    #         print(request.POST)
-        return redirect("/dashboard")
-
-def login(request): #POST
-    #  results = User.objects.filter(email=request.POST['email_form'])
-
-    #  if results: 
-    #      logged_user = results[0] 
-
-    #      if bcrypt.checkpw(request.POST['pw_form'].encode(), logged_user.password.encode()):
-
-    #         request.session['user_id'] = logged_user.id
-    #         return redirect("/dashboard")
-        
-    #      else:
-    #          messages.error(request, "The email and password do not match.")
-    #          return redirect("/") 
-    #  else:
-    #      messages.error(request, "This email has not been registered.") 
-         return redirect("/dashboard") # needs to be just redirect ("/") added /dashboard just for building out html
+def signin(request):
+    if request.method == "POST":
+        errors = User.objects.validate_login(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/signin')
+        else:
+            user = User.objects.get(email = request.POST['email'])
+            request.session['user_id'] = user.id
+            request.session['user_name'] = user.name
+            return redirect('/dashboard')
+    else:
+        return render(request, 'signin.html')
+    
+def register(request):
+    if request.method == "POST":
+        errors = User.objects.validate_user(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/register')
+        hashed_password = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
+        new_user = User.objects.create(first_name = request.POST['first_name'], last_name = request.POST['last_name'], email=request.POST['email'], password = hashed_password, user_level = 1)
+        request.session['user_id'] = new_user.id
+        request.session['user_name'] = new_user.first_name
+        return redirect('/dashboard')
+    else:
+        return render(request, 'register.html')
+def logoff(request):
+    request.session.flush()
+    return redirect('/')
 

d47c311aff9d992e56d7f0613407dd8eeef5be02
Michelle Barker <barkerwebdev47@gmail.com>
2020-07-23 19:57:18 -0700

    correctly placed urls into login_reg_app instead of dog_park_app. Michelle put in wrong spot to begin with.

--- apps/login_reg_app/urls.py
+++ apps/login_reg_app/urls.py
@@ -2,7 +2,13 @@ from django.urls import path, include
 from .import views
 
 urlpatterns = [
-    path('', views.index), #GET renders index.html for register or login
-    path('register', views.register), #POST redirect to index or redirect to /dashboard
-    path('login', views.login), #POST redirect to index or redirect to /dashboard
+    path('logout', views.logout), #GET
+    path('dashboard', views.dashboard), #GET renders dashboard.html - HOME
+    path('remove/<int:id>', views.remove), 
+    path('edit/<int:id>', views.edit), #GET renders edit.html
+    path('new', views.new), #GET renders to add a new trip
+    path('create', views.create), #POST creates (adds to database) a new trip and redirects to show_one/<int:id>
+    path('show_one/<int:id>', views.show_one), #GET renders show_one.html
+    path('update/<int:id>', views.update), #POST that redirects to /show_one/<int:id>
+    path('edit/update/<int:id>', views.update),
 ]

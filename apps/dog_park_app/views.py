313b0375e1edf189bf28606879fe5c752bccaaab
Irina Markov <irina@aquilatech.com>
2020-07-26 13:49:03 -0700

    added one line to views to import model User

--- apps/dog_park_app/views.py
+++ apps/dog_park_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import *
+from apps.login_reg_app.models import User
 
 def index(request):
     pass

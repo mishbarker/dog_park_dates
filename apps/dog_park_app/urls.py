c338a829fe6d2db17d085075cc5a0670d607ce13
Michelle Barker <barkerwebdev47@gmail.com>
2020-08-02 01:13:16 -0700

    Validations updated - still need time and way for user to change password and dog image validation. Style changes. Cleaned up code

--- apps/dog_park_app/urls.py
+++ apps/dog_park_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index),
     path('dashboard', views.dashboard), #GET renders dashboard.html - HOME
     path('playdates/new', views.new), #GET renders new.html to add a new playdate and POST to create a new playdate, may add a new park, in database, POST redirects back to dashboard.html
+    path('playdates/edit/<int:playdate_id>', views.edit_playdate),
     path('playdates/delete/<int:id>', views.delete_playdate),
     path('playdates/<int:id>', views.show_one),
     path('playdates/<int:playdate_id>/edit', views.edit_playdate),

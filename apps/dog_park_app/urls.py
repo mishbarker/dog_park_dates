b45ca2bd6f2e536301cc9b7812e7cb7bd7816d06
Michelle Barker <barkerwebdev47@gmail.com>
2020-08-02 01:31:55 -0700

    code clean-up before merging with development branch

--- apps/dog_park_app/urls.py
+++ apps/dog_park_app/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('dashboard', views.dashboard), #GET renders dashboard.html - HOME
-    path('playdates/new', views.new), #GET renders new.html to add a new playdate and POST to create a new playdate, may add a new park, in database, POST redirects back to dashboard.html
+    path('dashboard', views.dashboard),
+    path('playdates/new', views.new),
     path('playdates/edit/<int:playdate_id>', views.edit_playdate),
     path('playdates/delete/<int:id>', views.delete_playdate),
     path('playdates/<int:id>', views.show_one),
